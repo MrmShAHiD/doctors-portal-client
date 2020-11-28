8dbb84d6bb8432dc3fd32a9e0c125e7e13321c5d
SHAHID <43965492+MrmShAHiD@users.noreply.github.com>
2020-11-28 10:11:40 +0600

    navbar and banner done

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+import Banner from '../Banner/Banner';
+
+function Home() {
+    return (
+        <div>
+            <Banner/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
